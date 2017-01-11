b43550a1d6684b3ceb1a70213be946d40a2a699f
yohskeey <indecision_guy@hotmail.com>
2017-01-11 14:24:01 +0900

    PPAP

---
+++ PPAP/PPAP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PPAP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var ppap = new ppap01();
+            var ppap = new ppap02();
+            ppap.startPPAP();
+        }
+    }
+}
