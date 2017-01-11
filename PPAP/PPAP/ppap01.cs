8d0f2047615091f012d3c6d189f473b3c3c1abc5
yohskeey <indecision_guy@hotmail.com>
2017-01-11 14:25:09 +0900

    あげ忘れ

---
+++ PPAP/PPAP/ppap01.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAP
+{
+    class ppap01
+    {
+        public void startPPAP()
+        {
+            var pen = "ペン";
+            var pineapple = "パイナッポー";
+            var apple = "アッポー";
+            var successPPAP = new[] { pen, pineapple, apple, pen };   // 正答
+            var queuePPAP = new Queue<string>();
+            var rnd = new Random();
+            var countPPAP = 0;
+
+            while (true)
+            {
+                var i_have_a = pen;
+                var rNum = rnd.Next(0, 3);
+                switch (rNum)
+                {
+                    case 0:
+                        i_have_a = pineapple;
+                        break;
+                    case 1:
+                        i_have_a = apple;
+                        break;
+                }
+                Console.Write(i_have_a);
+
+                queuePPAP.Enqueue(i_have_a);
+                countPPAP++;
+
+                if (queuePPAP.Count > 4)
+                {
+                    //最新4件を保存する
+                    queuePPAP.Dequeue();
+                }
+
+                if (queuePPAP.ToArray().SequenceEqual(successPPAP))
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("\nペンパイナッポーアッポーペン！（I have {0} words）", countPPAP);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PPAP/PPAP/ppap02.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAP
+{
+    class ppap02
+    {
+        public void startPPAP()
+        {
+            var pen = "ペン";
+            var pai = "パイ";
+            var axtu = "アッ";
+            var natsu = "ナッ";
+            var po = "ポー";
+            var successPPAP = new[] { pen, pai, natsu, po, axtu, po, pen };   // 正答
+            var queuePPAP = new Queue<string>();
+            var rnd = new Random();
+            var countPPAP = 0;
+
+            while (true)
+            {
+                var have = pen;
+                var rNum = rnd.Next(0, 5);
+                switch (rNum)
+                {
+                    case 0:
+                        have = pai;
+                        break;
+                    case 1:
+                        have = axtu;
+                        break;
+                    case 2:
+                        have = natsu;
+                        break;
+                    case 3:
+                        have = po;
+                        break;
+                }
+                Console.Write(have);
+
+                queuePPAP.Enqueue(have);
+                countPPAP++;
+
+                if (queuePPAP.Count > 7)
+                {
+                    //最新5件を保存する
+                    queuePPAP.Dequeue();
+                }
+
+                if (queuePPAP.ToArray().SequenceEqual(successPPAP))
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("\nペンパイナッポーアッポーペン！（I have {0} words）", countPPAP);
+            Console.ReadKey();
+        }
+    }
+}
